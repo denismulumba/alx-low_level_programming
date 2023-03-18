23067dc0f4f660b7d1d3ff9d45ec38c1e85fc098
denismulumba <denismulumba@u.boisestate.edu>
2023-03-17 17:23:34 -0700

    Combinations

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - prints all possible combinations of single digit numbers
+ * 
+ * Return: 0
+ */
+int main(void)
+{
+	int num = 0;
+
+	while (num < 10)
+	{
+		putchar(num + '0');
+		if (num != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		num++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
