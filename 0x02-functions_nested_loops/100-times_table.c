f2eca2a2732bee5b6ddd873c871de8b2a045b1b9
denismulumba <denismulumba@u.boisestate.edu>
2023-03-21 20:56:51 -0700

    Multiplication tables for n

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_times_table - prints the n times table, starting with 0
+ *
+ * @n: is the number
+ * Return: the multiplication table
+ */
+void print_times_table(int n)
+{
+	int i, j, r;
+
+	if (n < 0 || n > 15)
+		return;
+
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <=n; j++)
+		{
+			r = i * j;
+			
+			if (j == 0)
+				printf("%d", r);
+			else
+				printf(", %3d", r);
+		}
+		printf("\n");
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -29,6 +29,8 @@ int add(int a, int b);
 
 void print_to_98(int n);
 
+void print_times_table(int n);
+
 int main(void);
 
 
