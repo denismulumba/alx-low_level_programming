234300ece7d7f5ffd2397cf1d8a1c4c95b9d6db6
denismulumba <denismulumba@u.boisestate.edu>
2023-03-21 20:45:12 -0700

    Added two new files

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main()
+{
+	int n1 = 1, n2 = 2, n3, count = 2;
+	printf("%d, %d, ", n1, n2);
+
+	while (count < 50)
+	{
+		n3 = n1 + n2;
+		printf("%d, ", n3);
+		n1 = n2;
+		n2 = n3;
+		count++;
+	}
+	n3 = n1 + n2;
+	print("%d\n", n3);
+	return (0);
+}
