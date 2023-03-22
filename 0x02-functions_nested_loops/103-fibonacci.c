3e98c5e51d55e5d3030c681e863e720c01f19700
denismulumba <denismulumba@u.boisestate.edu>
2023-03-21 20:33:37 -0700

    Even Liber Abbaci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry values
+ *
+ * Return; Always 0 (Success)
+ */
+int main()
+{
+	int n1 = 1, n2 = 2, n3, sum = 2;
+
+	while (n3 <= 4000000)
+	{
+		n3 = n1 + n2;
+		if (n3 % 2 == 0)
+		{
+			sum += n3;
+		}
+		n1 = n2;
+		n2 = n3;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
