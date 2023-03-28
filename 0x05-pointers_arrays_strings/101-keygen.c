127be50cc2d93b464f3c9e15763b943a87bc0416
denismulumba <denismulumba@u.boisestate.edu>
2023-03-27 18:22:00 -0700

    Debugging

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -2,24 +2,28 @@
 #include <stdlib.h>
 #include <time.h>
 
-int main()
+/**
+ * main - generates a random password of a specified length
+ *
+ * Return: Always 0.
+ */
+int main(void)
 {
     int counter = 0;
-    srandom(time(NULL));  // Correct seeding function for random()
+    srandom(time(NULL));  /* Correct seeding function for random() */
     char randChar;
 
-    int  passwordLength;
+    int passwordLength;
 
-    printf("Type in a password Length \n");
+    printf("Type in a password length\n");
     scanf("%d", &passwordLength);
 
-    while(counter < passwordLength)
+    while (counter < passwordLength)
     {
-        randChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"[random () % 62];
+        randChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"[random() % 62];
         printf("%c", randChar);
         counter++;
     }
-    printf("\n"); // Stops the output from being on the same line as the prompt
+    printf("\n"); /* Stops the output from being on the same line as the prompt */
     return 0;
 }
-
