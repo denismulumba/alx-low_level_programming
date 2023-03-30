645094fd950feb319c14decf68410080191784c5
denismulumba <denismulumba@u.boisestate.edu>
2023-03-30 08:12:35 -0700

    Cancatneting strings

---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This directory contains C programming codes that deal with more arrays, pointers and strings
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Included libraries */
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Function prototypes */
+char *_strcat(char *dest, char *src);
+
+#endif /* MAIN_H */
