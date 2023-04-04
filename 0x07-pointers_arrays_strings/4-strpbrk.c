c796c3328e2ec8203831fe808c5680864c031a13
denismulumba <denismulumba@u.boisestate.edu>
2023-04-04 08:38:23 -0700

    Searches a string for any of a set of bytes

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strpbrk - searches a string for any of a set of bytes.
+ * @s: pointer to the string to search in.
+ * @accept: pointer to the string containing the bytes to search for.
+ *
+ * Return: pointer to the byte in s that matches one of the bytes in accept,
+ *         or NULL if no such byte is found.
+ */
+char *_strpbrk(char *s, char *accept)
+{
+        int i, j;
+
+        for (i = 0; s[i] != '\0'; i++)
+        {
+                for (j = 0; accept[j] != '\0'; j++)
+                {
+                        if (s[i] == accept[j])
+                        {
+                                return (s + i);
+                        }
+                }
+        }
+
+        return (NULL);
+}
